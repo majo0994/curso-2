542cd0bc2a62b4b4838ccf7510956e7d75785a8d
Maria Jose Parejo <mariajoseparejodesousa@gmail.com>
2023-06-13 20:05:48 -0400

    navbar con menu mobile

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector('.email-nav-container');
-const desktopMenu = document.querySelector('.menu-container-1');
+const desktopMenu = document.querySelector('.menu-desktop');
+const iconMobileMenu = document.querySelector('.menu-mobile-nav');
+const mobileMenu = document.querySelector('.menu-mobile');
 
 navEmail.addEventListener('click', toggleDesktopMenu)
+iconMobileMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu() {
+   mobileMenu.classList.toggle('inactive')
 }
